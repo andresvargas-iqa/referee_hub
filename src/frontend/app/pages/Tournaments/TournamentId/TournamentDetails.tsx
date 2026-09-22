20d1939941943162f2cda375f08bb571986b7627
Andrés Vargas <anvargas84@gmail.com>
2026-09-22 10:33:56 +0200

    Add isVolunteerRegistrationOpen to tournament details

--- src/frontend/app/pages/Tournaments/TournamentId/TournamentDetails.tsx
+++ src/frontend/app/pages/Tournaments/TournamentId/TournamentDetails.tsx
@@ -606,6 +606,7 @@ const TournamentDetails = () => {
       organizer: tournament.organizer || "",
       isPrivate: tournament.isPrivate || false,
       isRegistrationOpen: tournament.isRegistrationOpen ?? true,
+      isVolunteerRegistrationOpen: tournament.isVolunteerRegistrationOpen ?? true,
       bannerImageUrl: tournament.bannerImageUrl || "",
     });
   };
